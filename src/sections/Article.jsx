ef4bcd9b4bbca7d5543e3bdd021e5d00656cf740
alvinirwanto <irwantoalvin@gmail.com>
2024-01-23 22:12:51 +0700

    add the articles

--- src/sections/Article.jsx
+++ src/sections/Article.jsx
@@ -27,13 +27,13 @@ export default function Article() {
         <section className='py-[5rem] text-white-primary pb-[15rem]'>
             <div className='flex flex-col xl:flex-row justify-end items-start xl:items-center gap-8 mb-[5rem]'>
                 <div className='bg-white-primary w-[25%] h-[2px] z-[20]' />
-                <h2 className='text-2xl md:text-4xl font-semibold uppercase font-space'>My Articles</h2>
+                <h2 className='text-2xl md:text-3xl xl:text-4xl font-semibold uppercase font-space'>My Articles</h2>
             </div>
-            <div className='grid grid-cols-2'>
+            <div className='grid grid-cols-1 xl:grid-cols-2 gap-10'>
                 <>
                     {
                         articles?.map((article, i) => (
-                            <Link key={i} href={article.link} target='_blank' className={i === 0 ? 'flex flex-col justify-between gap-4 !h-[40rem]' : 'hidden'}>
+                            <Link key={i} href={article.link} target='_blank' className={i === 0 ? 'flex flex-col justify-between gap-4 !h-[30rem] md:!h-[35rem] xl:!h-[40rem]' : 'hidden'}>
                                 {article.content && (
                                     <>
                                         {(() => {
@@ -42,7 +42,7 @@ export default function Article() {
                                             const firstImg = doc?.querySelector('img')?.getAttribute('src');
                                             return firstImg
                                                 ? <div className='relative h-[70%]'>
-                                                    <Image className='w-full h-full object-cover absolute bg-clip-content p-6 border-2 border-blue-primary rounded-lg' src={firstImg} height={500} width={500} alt='thumbnail' />
+                                                    <Image className='w-full h-full object-cover absolute bg-clip-content p-4 md:p-6 border-2 border-blue-primary rounded-lg' src={firstImg} height={500} width={500} alt='thumbnail' />
                                                     <div className={`w-full h-full bg-gradient-to-l from-[#61dcfb4b] to-transparent absolute rounded-lg`}></div>
                                                 </div>
                                                 : null;
@@ -58,7 +58,7 @@ export default function Article() {
                                                 const doc = parser.parseFromString(article.content, 'text/html');
                                                 const firstParagraph = doc.querySelector('p');
                                                 return firstParagraph ? (
-                                                    <p className='line-clamp-2 text-lg'>{firstParagraph.textContent}</p>
+                                                    <p className='line-clamp-2 text-sm md:text-lg'>{firstParagraph.textContent}</p>
                                                 ) : (
                                                     null
                                                 );
@@ -68,7 +68,7 @@ export default function Article() {
                                     <div className='flex justify-start gap-6'>
                                         {
                                             article?.category?.map((category, i) => (
-                                                <div key={i} className='text-blue-primary capitalize text-lg'>{category}</div>
+                                                <div key={i} className='text-blue-primary capitalize text-xs md:text-lg'>{category}</div>
                                             ))
                                         }
                                     </div>
@@ -78,10 +78,10 @@ export default function Article() {
                     }
                 </>
                 {/* i === articles.length - 1 || */}
-                <div className='ml-10 flex flex-col justify-between gap-6 !h-[40rem]'>
+                <div className='xl:ml-10 flex flex-col justify-between gap-6 h-[30rem] md:!h-[40rem]'>
                     {
                         articles?.map((article, i) => (
-                            <Link key={i} href={article.link} target='_blank' className={i === 0 ? 'hidden' : 'grid grid-cols-2 gap-4 h-[33%]'}>
+                            <Link key={i} href={article.link} target='_blank' className={i === 0 ? 'hidden' : 'grid grid-cols-[1fr_1.7fr] md:grid-cols-2 gap-4 !h-[33%]'}>
                                 {article.content && (
                                     <>
                                         {(() => {
@@ -90,7 +90,7 @@ export default function Article() {
                                             const firstImg = doc?.querySelector('img')?.getAttribute('src');
                                             return firstImg
                                                 ? <div className='relative h-full'>
-                                                    <Image className='w-full h-full object-cover absolute bg-clip-content p-4 border-2 border-blue-primary rounded-lg' src={firstImg} height={500} width={500} alt='thumbnail' />
+                                                    <Image className='w-full h-full object-cover absolute bg-clip-content p-2 md:p-4 border-2 border-blue-primary rounded-lg' src={firstImg} height={500} width={500} alt='thumbnail' />
                                                     <div className={`w-full h-full bg-gradient-to-l from-[#61dcfb4b] to-transparent absolute rounded-lg`}></div>
                                                 </div>
                                                 : null;
@@ -99,7 +99,7 @@ export default function Article() {
                                 )}
                                 <div className='flex flex-col justify-between gap-4 py-3'>
                                     <div className='flex flex-col gap-4'>
-                                        <span className='font-semibold text-lg line-clamp-2'>{article.title}</span>
+                                        <span className='font-semibold text-sm md:text-lg line-clamp-2'>{article.title}</span>
                                         {article.content && (
                                             <>
                                                 {(() => {
@@ -107,7 +107,7 @@ export default function Article() {
                                                     const doc = parser.parseFromString(article.content, 'text/html');
                                                     const firstParagraph = doc.querySelector('p');
                                                     return firstParagraph ? (
-                                                        <p className='line-clamp-3'>{firstParagraph.textContent}</p>
+                                                        <p className='text-xs md:text-base line-clamp-3'>{firstParagraph.textContent}</p>
                                                     ) : (
                                                         null
                                                     );
@@ -118,7 +118,7 @@ export default function Article() {
                                     <div className='flex justify-between'>
                                         {
                                             article?.category?.map((category, i) => (
-                                                <div key={i} className='text-blue-primary capitalize'>{category}</div>
+                                                <div key={i} className={`${i >= 3 ? 'hidden': 'text-blue-primary text-xs md:text-sm xl:text-base capitalize'}`}>{category}</div>
                                             ))
                                         }
                                     </div>
