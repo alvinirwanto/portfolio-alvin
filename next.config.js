8ef205afe45924e9117e7a76c793f763e35282d3
Alvin Irwanto <irwantoalvin@gmail.com>
2026-04-10 00:53:19 +0700

    feat - adjust next config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,16 @@ const nextConfig = {
             'medium.com',
             'cdn-images-1.medium.com'
         ]
-    }
+    },
+    webpack: (config, { isServer }) => {
+        // This tells Webpack to ignore 'fs' when bundling for the client/browser
+        if (!isServer) {
+            config.resolve.fallback = {
+                fs: false,
+            };
+        }
+        return config;
+    },
 }
 
 module.exports = nextConfig
